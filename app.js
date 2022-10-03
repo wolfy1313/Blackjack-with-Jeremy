b453b34c5593cdd7023ad91d53a1d3acd1072418
jeremyHarrell <hillvulf@gmail.com>
2022-10-03 09:54:40 -0500

    added divs to gameplay.html and started creating game pieces in js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+const playerScore = document.getElementById('playerScoreBoard')
+const dealerScore = document.getElementById('dealerScoreBoard')
+const pushScore = document.getElementById('pushScoreBoard')
+
+let playerWins = 0
+let dealerWins = 0
+let pushWins = 0
+
+const resetButton = document.getElementById('reset')
+
+let isPlayerTurn = true
+let isWinner = false
+let isPush = false
+
+let playerHand = []
+let dealerHand = []
+
+const suits = ['hearts', 'spades', 'clubs', 'diamonds']
+const cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+const deck = [suits, cards]
+console.log(deck)
 
