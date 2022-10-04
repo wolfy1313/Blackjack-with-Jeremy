283df7404b9171544b85c9fd15435df8bc1700ca
jeremyHarrell <hillvulf@gmail.com>
2022-10-04 12:38:39 -0500

    created playerBlackJack function and checkBust function

--- app.js
+++ app.js
@@ -25,266 +25,288 @@ let isPush = false
 let playerHand = []
 let dealerHand = []
 
+const playerBlackJack = () => {
+  if (playerScore === 21){
+    alert("PLAYER HAS BLACKJACK")
+    isPlayerTurn = !isPlayerTurn
+  } else if (dealerScore === 21) {
+    alert("DEALER HAS BLACKJACK")
+  }
+}
+
+const checkBust = () => {
+  if (playerScore > 21) {
+    alert("PLAYER BUSTED AND GAME IS OVER CLICK PLAY AGAIN")
+  }
+}
+
+const checkAce = () => {
+  if (playerScore <12) {
+    playerHand.forEach(card => {
+
+    })
+  }
+}
 
 const deck =[
   {
-    name: 'Ace of Spades',
+    name: 'Ace',
     value: 11,
     suit: 'spades',
-    // art: //img src=""//
+    // art: placeholder,
   },
   {
-    name: 'Ace of Clubs',
+    name: 'Ace',
     value: 11,
     suit: 'clubs'
   },
   {
-    name: 'Ace of Hearts',
+    name: 'Ace',
     value: 11,
     suit: 'hearts'
   },
   {
-    name: 'Ace of Diamonds',
+    name: 'Ace',
     value: 11,
     suit: 'diamonds'
   },
   {
-    name: 'King of Clubs',
+    name: 'King',
     value: 10,
     suit: 'clubs'
   },
   {
-    name: 'Queen of Clubs',
+    name: 'Queen',
     value: 10,
     suit: 'clubs'
   },
   {
-    name: 'Jack of Clubs',
+    name: 'Jack',
     value: 10,
     suit: 'clubs'
   },
   {
-    name: '10 of Clubs',
+    name: '10',
     value: 10,
     suit: 'clubs'
   },
   {
-    name: '9 of Clubs',
+    name: '9',
     value: 9,
     suit: 'clubs'
   },
   {
-    name: '8 of Clubs',
+    name: '8',
     value: 8,
     suit: 'clubs'
   },
   {
-    name: '7 of Clubs',
+    name: '7',
     value: 7,
     suit: 'clubs'
   },
   {
-    name: '6 of Clubs',
+    name: '6',
     value: 6,
     suit: 'clubs'
   },
   {
-    name: '5 of Clubs',
+    name: '5',
     value: 5,
     suit: 'clubs'
   },
   {
-    name: '4 of Clubs',
+    name: '4',
     value: 4,
     suit: 'clubs'
   },
   {
-    name: '3 of Clubs',
+    name: '3',
     value: 3,
     suit: 'clubs'
   },
   {
-    name: '2 of Clubs',
+    name: '2',
     value: 2,
     suit: 'clubs'
   },
   {
-    name: 'King of Spades',
+    name: 'King',
     value: 10,
     suit: 'spades'
   },
   {
-    name: 'Queen of Spades',
+    name: 'Queen',
     value: 10,
     suit: 'spades'
   },
   {
-    name: 'Jack of Spades',
+    name: 'Jack',
     value: 10,
     suit: 'spades'
   },
   {
-    name: '10 of Spades',
+    name: '10',
     value: 10,
     suit: 'spades'
   },
   {
-    name: '9 of Spades',
+    name: '9',
     value: 9,
     suit: 'spades'
   },
   {
-    name: '8 of Spades',
+    name: '8',
     value: 8,
     suit: 'spades'
   },
   {
-    name: '7 of Spades',
+    name: '7',
     value: 7,
     suit: 'spades'
   },
   {
-    name: '6 of Spades',
+    name: '6',
     value: 6,
     suit: 'spades'
   },
   {
-    name: '5 of Spades',
+    name: '5',
     value: 5,
     suit: 'spades'
   },
   {
-    name: '4 of Spades',
+    name: '4',
     value: 4,
     suit: 'spades'
   },
   {
-    name: '3 of Spades',
+    name: '3',
     value: 3,
     suit: 'spades'
   },
   {
-    name: '2 of Spades',
+    name: '2',
     value: 2,
     suit: 'spades'
   },
   {
-    name: 'King of Diamonds',
+    name: 'King',
     value: 10,
     suit: 'diamonds'
   },
   {
-    name: 'Queen of Diamonds',
+    name: 'Queen',
     value: 10,
     suit: 'diamonds'
   },
   {
-    name: 'Jack of Diamonds',
+    name: 'Jack',
     value: 10,
     suit: 'diamonds'
   },
   {
-    name: '10 of Diamonds',
+    name: '10',
     value: 10,
     suit: 'diamonds'
   },
   {
-    name: '9 of Diamonds',
+    name: '9',
     value: 9,
     suit: 'diamonds'
   },
   {
-    name: '8 of Diamonds',
+    name: '8',
     value: 8,
     suit: 'diamonds'
   },
   {
-    name: '7 of Diamonds',
+    name: '7',
     value: 7,
     suit: 'diamonds'
   },
   {
-    name: '6 of Diamonds',
+    name: '6',
     value: 6,
     suit: 'diamonds'
   },
   {
-    name: '5 of Diamonds',
+    name: '5',
     value: 5,
     suit: 'diamonds'
   },
   {
-    name: '4 of Diamonds',
+    name: '4',
     value: 4,
     suit: 'diamonds'
   },
   {
-    name: '3 of Diamonds',
+    name: '3',
     value: 3,
     suit: 'diamonds'
   },
   {
-    name: '2 of Diamonds',
+    name: '2',
     value: 2,
     suit: 'diamonds'
   },
   {
-    name: 'King of Hearts',
+    name: 'King',
     value: 10,
     suit: 'hearts'
   },
   {
-    name: 'Queen of Hearts',
+    name: 'Queen',
     value: 10,
     suit: 'hearts'
   },
   {
-    name: 'Jack of Hearts',
+    name: 'Jack',
     value: 10,
     suit: 'hearts'
   },
   {
-    name: '10 of Hearts',
+    name: '10',
     value: 10,
     suit: 'hearts'
   },
   {
-    name: '9 of Hearts',
+    name: '9',
     value: 9,
     suit: 'hearts'
   },
   {
-    name: '8 of Hearts',
+    name: '8',
     value: 8,
     suit: 'hearts'
   },
   {
-    name: '7 of Hearts',
+    name: '7',
     value: 7,
     suit: 'hearts'
   },
   {
-    name: '6 of Hearts',
+    name: '6',
     value: 6,
     suit: 'hearts'
   },
   {
-    name: '5 of Hearts',
+    name: '5',
     value: 5,
     suit: 'hearts'
   },
   {
-    name: '4 of Hearts',
+    name: '4',
     value: 4,
     suit: 'hearts'
   },
   {
-    name: '3 of Hearts',
+    name: '3',
     value: 3,
     suit: 'hearts'
   },
   {
-    name: '2 of Hearts',
+    name: '2',
     value: 2,
     suit: 'hearts'
   },
@@ -300,11 +322,12 @@ const shuffleDeck = () => {
 
 const countCards = () => {
   playerHand.forEach(pCard => {
-    playerScore += pCard.value
+    playerScore = pCard.value
   });
   dealerHand.forEach(dCard => {
     dealerScore += dCard.value
   })
+  playerBlackJack()
 }
 
 const dealGame = () => {
@@ -312,6 +335,7 @@ const dealGame = () => {
   playerHand.push(deck.pop());
   playerHand.push(deck.pop());
   countCards()
+  checkBust()
   console.log(playerScore)
   console.log(playerHand)
   // console.log(playerHand[0].value)
@@ -330,6 +354,7 @@ const dealGame = () => {
   dealerHand.push(deck.pop())
   countCards()
   console.log(dealerScore)
+  console.log(dealerHand)
   // console.log(dealerHand)
 }
 
@@ -338,15 +363,11 @@ const dealGame = () => {
 const playerHit = () => {
   playerHand.push(deck.pop())
   countCards()
+  checkBust()
   console.log(playerScore)
   console.log(playerHand)
 }
-const playerBlackJack = () => {
-  if (playerScore=== 21){
-    alert("Player Wins")
-    isPlayerTurn = !isPlayerTurn
-  }
-}
+
 
 const playerStay = () => {
   isDealerTurn = true
@@ -360,6 +381,8 @@ const resetGame = () => {
   isDealerTurn = false
   isWinner = false
   isPush = false
+  playerHand = []
+  dealerHand= []
 }
 
 
