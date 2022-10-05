b10b0a3a28209cdf25580e23c5b671acb17a4813
jeremyHarrell <hillvulf@gmail.com>
2022-10-05 11:19:21 -0500

    added ids to card objects in deck array

--- app.js
+++ app.js
@@ -134,263 +134,314 @@ const deck =[
     name: 'Ace',
     value: 11,
     suit: 'spades',
-    // id: placeholder,
+    id: 'Ace of spades',
     // art: placeholder,
   },
   {
     name: 'Ace',
     value: 11,
-    suit: 'clubs'
+    suit: 'clubs',
+    id: 'Ace of clubs',
   },
   {
     name: 'Ace',
     value: 11,
-    suit: 'hearts'
+    suit: 'hearts',
+    id: 'Ace of hearts',
   },
   {
     name: 'Ace',
     value: 11,
-    suit: 'diamonds'
+    suit: 'diamonds',
+    id: 'Ace of diamonds',
   },
   {
     name: 'King',
     value: 10,
-    suit: 'clubs'
+    suit: 'clubs',
+    id: 'King of  clubs',
   },
   {
     name: 'Queen',
     value: 10,
-    suit: 'clubs'
+    suit: 'clubs',
+    id: 'Queen of clubs',
   },
   {
     name: 'Jack',
     value: 10,
-    suit: 'clubs'
+    suit: 'clubs',
+    id: 'Jack of clubs',
   },
   {
     name: '10',
     value: 10,
-    suit: 'clubs'
+    suit: 'clubs',
+    id: '10 of clubs',
   },
   {
     name: '9',
     value: 9,
-    suit: 'clubs'
+    suit: 'clubs',
+    id: '9 of clubs',
   },
   {
     name: '8',
     value: 8,
-    suit: 'clubs'
+    suit: 'clubs',
+    id: '8 of clubs',
   },
   {
     name: '7',
     value: 7,
-    suit: 'clubs'
+    suit: 'clubs',
+    id: '7 of clubs',
   },
   {
     name: '6',
     value: 6,
-    suit: 'clubs'
+    suit: 'clubs',
+    id: '6 of clubs',
   },
   {
     name: '5',
     value: 5,
-    suit: 'clubs'
+    suit: 'clubs',
+    id: '5 of clubs',
   },
   {
     name: '4',
     value: 4,
-    suit: 'clubs'
+    suit: 'clubs',
+    id: '4 of clubs',
   },
   {
     name: '3',
     value: 3,
-    suit: 'clubs'
+    suit: 'clubs',
+    id: '3 of clubs',
   },
   {
     name: '2',
     value: 2,
-    suit: 'clubs'
+    suit: 'clubs',
+    id: '2 of clubs',
   },
   {
     name: 'King',
     value: 10,
-    suit: 'spades'
+    suit: 'spades',
+    id: 'King of spades',
   },
   {
     name: 'Queen',
     value: 10,
-    suit: 'spades'
+    suit: 'spades',
+    id: 'Queen of spades',
   },
   {
     name: 'Jack',
     value: 10,
-    suit: 'spades'
+    suit: 'spades',
+    id: 'Jack of spades',
   },
   {
     name: '10',
     value: 10,
-    suit: 'spades'
+    suit: 'spades',
+    id: '10 of spades',
   },
   {
     name: '9',
     value: 9,
-    suit: 'spades'
+    suit: 'spades',
+    id: '9 of spades',
   },
   {
     name: '8',
     value: 8,
-    suit: 'spades'
+    suit: 'spades',
+    id: '8 of spades',
   },
   {
     name: '7',
     value: 7,
-    suit: 'spades'
+    suit: 'spades',
+    id: '7 of spades',
   },
   {
     name: '6',
     value: 6,
-    suit: 'spades'
+    suit: 'spades',
+    id: '6 of spades',
   },
   {
     name: '5',
     value: 5,
-    suit: 'spades'
+    suit: 'spades',
+    id: '5 of spades',
   },
   {
     name: '4',
     value: 4,
-    suit: 'spades'
+    suit: 'spades',
+    id: '4 of spades',
   },
   {
     name: '3',
     value: 3,
-    suit: 'spades'
+    suit: 'spades',
+    id: '3 of spades',
   },
   {
     name: '2',
     value: 2,
-    suit: 'spades'
+    suit: 'spades',
+    id: '2 of spades',
   },
   {
     name: 'King',
     value: 10,
-    suit: 'diamonds'
+    suit: 'diamonds',
+    id: 'King of diamonds',
   },
   {
     name: 'Queen',
     value: 10,
-    suit: 'diamonds'
+    suit: 'diamonds',
+    id: 'Queen of diamonds',
   },
   {
     name: 'Jack',
     value: 10,
-    suit: 'diamonds'
+    suit: 'diamonds',
+    id: 'Jack of diamonds',
   },
   {
     name: '10',
     value: 10,
-    suit: 'diamonds'
+    suit: 'diamonds',
+    id: '10 of diamonds',
   },
   {
     name: '9',
     value: 9,
-    suit: 'diamonds'
+    suit: 'diamonds',
+    id: '9 of diamonds',
   },
   {
     name: '8',
     value: 8,
-    suit: 'diamonds'
+    suit: 'diamonds',
+    id: '8 of diamonds',
   },
   {
     name: '7',
     value: 7,
-    suit: 'diamonds'
+    suit: 'diamonds',
+    id: '7 of diamonds',
   },
   {
     name: '6',
     value: 6,
-    suit: 'diamonds'
+    suit: 'diamonds',
+    id: '6 of diamonds',
   },
   {
     name: '5',
     value: 5,
-    suit: 'diamonds'
+    suit: 'diamonds',
+    id: '5 of diamonds',
   },
   {
     name: '4',
     value: 4,
-    suit: 'diamonds'
+    suit: 'diamonds',
+    id: '4 of diamonds',
   },
   {
     name: '3',
     value: 3,
-    suit: 'diamonds'
+    suit: 'diamonds',
+    id: '3 of diamonds',
   },
   {
     name: '2',
     value: 2,
-    suit: 'diamonds'
+    suit: 'diamonds',
+    id: '2 of diamonds',
   },
   {
     name: 'King',
     value: 10,
-    suit: 'hearts'
+    suit: 'hearts',
+    id: 'King of hearts',
   },
   {
     name: 'Queen',
     value: 10,
-    suit: 'hearts'
+    suit: 'hearts',
+    id: 'Queen of hearts',
   },
   {
     name: 'Jack',
     value: 10,
-    suit: 'hearts'
+    suit: 'hearts',
+    id: 'Jack of hearts',
   },
   {
     name: '10',
     value: 10,
-    suit: 'hearts'
+    suit: 'hearts',
+    id: '10 of hearts',
   },
   {
     name: '9',
     value: 9,
-    suit: 'hearts'
+    suit: 'hearts',
+    id: '9 of hearts',
   },
   {
     name: '8',
     value: 8,
-    suit: 'hearts'
+    suit: 'hearts',
+    id: '8 of hearts',
   },
   {
     name: '7',
     value: 7,
-    suit: 'hearts'
+    suit: 'hearts',
+    id: '7 of hearts',
   },
   {
     name: '6',
     value: 6,
-    suit: 'hearts'
+    suit: 'hearts',
+    id: '6 of hearts',
   },
   {
     name: '5',
     value: 5,
-    suit: 'hearts'
+    suit: 'hearts',
+    id: '5 of hearts',
   },
   {
     name: '4',
     value: 4,
-    suit: 'hearts'
+    suit: 'hearts',
+    id: '4 of hearts',
   },
   {
     name: '3',
     value: 3,
-    suit: 'hearts'
+    suit: 'hearts',
+    id: '3 of hearts',
   },
   {
     name: '2',
     value: 2,
-    suit: 'hearts'
+    suit: 'hearts',
+    id: '2 of hearts',
   },
 ]
 
@@ -460,6 +511,8 @@ const playerHit = () => {
 
 const dealerPlay = () => {
   dealButton.removeEventListener('click', dealGame)
+  hitButton.removeEventListener('click', playerHit)
+  // stayButton.removeEventListener('click', dealerPlay)
   isPlayerTurn = false
   isActiveGame = true
   console.log("dealerPlay, playerScore", playerScore)
@@ -468,6 +521,16 @@ const dealerPlay = () => {
     alert("DEALER WINS HIT PLAY AGAIN TO PLAY...AGAIN")
     dealerWins++
     dealerTally.innerText = dealerWins
+    isActiveGame = false
+    return
+  } else if (playerScore > 21){
+    alert("PLAYER ALREADY LOST THERE IS NO STAYING")
+    return
+  } 
+  else if (playerScore === dealerScore && dealerScore >= 17){
+    alert("PUSH IT")
+    pushWins++
+    pushTally.innerText = pushWins
     isActiveGame = false
     return
   }
@@ -489,6 +552,7 @@ const dealerPlay = () => {
 
 const resetGame = () => {
   dealButton.addEventListener('click', dealGame)
+  hitButton.addEventListener('click', playerHit)
   isActiveGame = false
   playerScore = 0
   dealerScore = 0
